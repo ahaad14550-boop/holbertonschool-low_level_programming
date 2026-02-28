32bf62037c58a983cb7591dba9f80e8954790b6a
ahaad14550-boop <ahaad.14550@gmail.com>
2026-02-28 19:08:27 +0000

    Add task 10n

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,26 +2,19 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the number to extract the last digit from
+ * @n: the number
  *
- * Return: value of the last digit
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last;
 
-	/* استخراج الرقم الأخير باستخدام باقي القسمة */
-	last_digit = n % 10;
+	if (n < 0)
+		last = -(n % 10);
+	else
+		last = n % 10;
 
-	/* تحويل النتيجة لموجب إذا كان الرقم الأصلي سالباً */
-	if (last_digit < 0)
-	{
-		last_digit = last_digit * -1;
-	}
-
-	/* طباعة الرقم باستخدام _putchar */
-	_putchar(last_digit + '0');
-
-	/* إعادة قيمة الرقم الأخير */
-	return (last_digit);
+	_putchar('0' + last);
+	return (last);
 }
