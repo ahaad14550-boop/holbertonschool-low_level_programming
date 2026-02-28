668d49cea6c7782848874dff4bf574835bf22b46
ahaad14550-boop <ahaad.14550@gmail.com>
2026-02-28 16:55:28 +0000

    Update main.h with _islower prototype

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* بروتوتايب الدالة الأساسية للطباعة */
 int _putchar(char c);
+
+/* بروتوتايب المهام السابقة */
 void print_alphabet(void);
 void print_alphabet_x10(void);
+
+/* بروتوتايب المهمة الحالية */
+int _islower(int c);
 
 #endif
