f145d1aa2a8a0828ba858b7743c070e006746fe0
ahaad14550-boop <ahaad.14550@gmail.com>
2026-02-28 17:44:40 +0000

    Add task 10n

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int); /* أضف هذا السطر هنا */
-
-#endif
+void print_to_98(int n);
