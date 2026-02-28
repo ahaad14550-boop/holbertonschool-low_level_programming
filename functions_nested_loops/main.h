35e768daf5114da3cf1a50238debf3097e71d94f
ahaad14550-boop <ahaad.14550@gmail.com>
2026-02-28 16:59:05 +0000

    Add task 4: _isalpha function

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,14 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/* بروتوتايب الدالة الأساسية للطباعة */
 int _putchar(char c);
-
-/* بروتوتايب المهام السابقة */
 void print_alphabet(void);
 void print_alphabet_x10(void);
-
-/* بروتوتايب المهمة الحالية */
 int _islower(int c);
-
-#endif
+int _isalpha(int c); /* أضف هذا السطر هنا */
