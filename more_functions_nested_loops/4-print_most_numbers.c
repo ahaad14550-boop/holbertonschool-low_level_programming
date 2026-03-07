e3a1c82d9e486a249b080da301b6e20352ddf2cd
root <root@ip-10-42-194-220.ec2.internal>
2026-03-07 00:56:36 +0000

    putchar use

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line.
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		/* نتحقق أن الرقم ليس 2 وليس 4 قبل الطباعة */
+		if (i != '2' && i != '4')
+		{
+			_putchar(i); /* الاستخدام الأول */
+		}
+	}
+	_putchar('\n'); /* الاستخدام الثاني */
+}
+
