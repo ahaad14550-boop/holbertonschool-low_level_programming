e773a1ddfac77fadeb118bbeb1209a6d31434815
root <ahaad.14550@gmail.com>
2026-03-14 07:18:49 +0000

    h

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - يعيد طول السلسلة النصية باستخدام التكرار
- * @s: مؤشر (pointer) للسلسلة النصية
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
  *
- * Return: طول النص (integer)
+ * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	/* Base Case: إذا وصلنا لنهاية النص */
 	if (*s == '\0')
 	{
 		return (0);
 	}
-
-	/* التكرار: 1 + استدعاء الدالة للحرف التالي */
 	return (1 + _strlen_recursion(s + 1));
 }
