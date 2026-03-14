b9a3380ebf7122eadfd00647f08125c9c4c09266
root <ahaad.14550@gmail.com>
2026-03-14 07:33:37 +0000

    h

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,10 +3,10 @@
 int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to calculate the square root of.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Return: The resulting square root.
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
@@ -16,11 +16,12 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * actual_sqrt_recursion - Recursively finds the natural square root of a number.
- * @n: The number to calculate the square root of.
- * @i: The iterator to test.
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
  *
- * Return: The resulting square root.
+ * Return: the resulting square root
  */
 int actual_sqrt_recursion(int n, int i)
 {
