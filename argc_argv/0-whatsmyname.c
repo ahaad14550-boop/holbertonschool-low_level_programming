b1882caffb81a901d0000c12fb8df7264423a43a
ahaad14550-boop <ahaad.14550@gmail.com>
2026-03-14 11:08:57 +0000

    h

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,17 @@
 
 /**
  * main - prints the name of the program, followed by a new line
- * @argc: number of arguments (unused in this task)
+ * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	(void)argc; 
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
 
 	return (0);
 }
