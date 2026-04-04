409b4f8126ed3217f6fcbea561ac61046801be8d
ahaad14550-boop <ahaad.14550@gmail.com>
2026-04-04 18:50:18 +0000

    h

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -2,7 +2,11 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
-
-
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+
+
