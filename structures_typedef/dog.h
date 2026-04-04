6a555b0b44d05614d786f6d802d234f278a7bd22
ahaad14550-boop <ahaad.14550@gmail.com>
2026-04-04 18:37:12 +0000

    h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,12 +3,9 @@
 
 /**
  * struct dog - a struct that creates a dog info type
- * @name: First member (char *)
- * @age: Second member (float)
- * @owner: Third member (char *)
- *
- * Description: This structure stores basic information about a dog,
- * including its name, age, and who owns it.
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
@@ -17,4 +14,14 @@ struct dog
 	char *owner;
 };
 
-#endif /* DOG_H */
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
