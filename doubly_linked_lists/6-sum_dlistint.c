fbba0862f0b1eac14ef8d796d483da01fef00dfe
ahaad14550-boop <ahaad.14550@gmail.com>
2026-04-25 15:57:36 +0000

    Fix sum_dlistint prototype and add function

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,24 +1,20 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
- * @head: pointer to the head of the list
- * @index: index of the node to return, starting from 0
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list
+ * @head: pointer to the head of the doubly linked list
  *
- * Return: the nth node, or NULL if the node does not exist
+ * Return: the sum of all data, or 0 if the list is empty
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+int sum_dlistint(dlistint_t *head)
 {
-	unsigned int i = 0;
+	int sum = 0;
 
 	while (head != NULL)
 	{
-		if (i == index)
-			return (head);
-		
+		sum += head->n;
 		head = head->next;
-		i++;
 	}
 
-	return (NULL);
+	return (sum);
 }
