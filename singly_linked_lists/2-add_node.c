f45aa042a12b4ff5a3033640c09296aa2eced7ea
ahaad14550-boop <ahaad.14450@gmail.com>
2026-04-25 14:02:19 +0000

    h

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,7 +14,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	unsigned int len = 0;
 
-	while (str[len])
+	while (str && str[len])
 		len++;
 
 	new_node = malloc(sizeof(list_t));
@@ -27,6 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->len = len;
 	new_node->next = (*head);
 	(*head) = new_node;
