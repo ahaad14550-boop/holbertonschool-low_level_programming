a071881ad2507d83fcae1abb34c413bb6fba1a88
ahaad14550-boop <ahaad.14450@gmail.com>
2026-04-25 15:32:24 +0000

    Final fix for task 2

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -12,7 +12,6 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -21,19 +20,10 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-
 size_t print_dlistint(const dlistint_t *h);
-
-
 size_t dlistint_len(const dlistint_t *h);
-
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-
-
 void free_dlistint(dlistint_t *head);
-size_t dlistint_len(const dlistint_t *h);
-#endif 
+
+#endif /* LISTS_H */ 
