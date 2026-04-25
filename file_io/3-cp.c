6773efce8d6d37b472d6c9207c568be220d9adbf
ahaad14550-boop <ahaad.14550@gmail.com>
2026-04-25 16:48:51 +0000

    h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,6 +4,19 @@
 #include <fcntl.h>
 
 /**
+ * close_fd - Closes a file descriptor and exits on failure.
+ * @fd: The file descriptor to close.
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - Copies the content of a file to another file.
  * @ac: The number of arguments.
  * @av: An array of pointers to the arguments.
@@ -20,25 +33,19 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	/* Open source file */
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
-	/* Open/Create destination file with rw-rw-r-- (0664) */
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close(fd_from);
+		close_fd(fd_from);
 		exit(99);
 	}
-
-	/* Read and Write loop using 1024 byte buffer */
 	while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
 	{
 		w_bytes = write(fd_to, buffer, r_bytes);
@@ -48,25 +55,12 @@ int main(int ac, char **av)
 			exit(99);
 		}
 	}
-
 	if (r_bytes == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
-	/* Close file descriptors and check for errors */
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	close_fd(fd_from);
+	close_fd(fd_to);
 	return (0);
 }
